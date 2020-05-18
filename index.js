4f8590d392d68296907605c3f6f40cf5b3cfe0a4
Konstantin Vyatkin <tino@vtkn.io>
2020-05-18 02:32:15 -0400

    unblock client on destroy

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-import { RedisReadableStream } from './resp3/readable.js';
-export async function connect() {
-  const stream = new RedisReadableStream('helloStream');
-  stream.on('error', console.error.bind(console));
-  let i = 0;
-  for await (const data of stream) {
-    i++;
-    console.log(data);
-    if (i == 3) break;
-  }
-  stream.destroy();
-}
-
-connect();
+export * from './resp3/readable.js';
+export * from './resp3/writable.js';
